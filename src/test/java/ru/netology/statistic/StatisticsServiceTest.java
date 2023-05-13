c501b830bc54b4f97ddf5be320e48643272f456c
Tervi test <Tervi@list.ru>
2023-05-13 09:57:44 +0300

    set up jacoco

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,11 +1,9 @@
 package ru.netology.statistic;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.Assertions;
-
 public class StatisticsServiceTest {
-
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
@@ -19,11 +17,11 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMaxFromMiddle() {
+    void findMaxSetup() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 80, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 55, 8, 4, 5, 3, 8, 6, 11, 11, 12, 12};
+        long expected = 55;
 
         long actual = service.findMax(incomesInBillions);
 
